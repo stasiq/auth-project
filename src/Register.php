e6219bdd0dc3638dcd0d6dafcf47af27e86d10aa
s.konovalov <s.konovalov@intensa.ru>
2022-08-17 11:01:54 +0300

    Метод регистрации в работе

--- src/Register.php
+++ src/Register.php
@@ -8,6 +8,7 @@ class Register
     private static $email;
     private static $password;
     private static $password_confirm;
+    private static $errors = [];
 
     public function __construct($username, $email, $password, $password_confirm)
     {
@@ -17,11 +18,23 @@ class Register
         self::$password_confirm = $password_confirm;
     }
 
-    public function validatePass()
+    private static function validatePass()
     {
         if (self::$password !== self::$password_confirm) {
-            $_SESSION['message'] = 'Пароли не совпадают';
-            header('Location: ../register.php');
+            $_SESSION['message'][] = 'Пароли не совпадают';
+            self::$errors[] = 'password_confirm';
         };
+    }
+
+    public static function registration()
+    {
+        if (empty(self::$errors)) {
+            $db = Db::getConnect();
+            $sql = 'INSERT INTO `users` (`name`, `email`, `admin`, `password`)
+VALUES ("self::$username", "self::$email ", NULL, "self::$password");';
+            $db->query($sql);
+            header('Location: ../register.php');
+        }
+
     }
 }
